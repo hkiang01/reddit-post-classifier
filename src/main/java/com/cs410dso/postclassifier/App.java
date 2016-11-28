802e7d9f06d897e19d868a1140664f696e785f82
hkiang01 <hkiang01@yahoo.com>
2016-11-27 21:44:48 -0500

    successfully reading into a Spark dataframe

--- src/main/java/com/cs410dso/postclassifier/App.java
+++ src/main/java/com/cs410dso/postclassifier/App.java
@@ -1,36 +1,38 @@
 package com.cs410dso.postclassifier;
 
 import com.cs410dso.postclassifier.ingestion.FilteredSubredditIngestion;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.json.simple.JSONObject;
+
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 /**
  * App main
  */
 public class App {
     public static void main(String[] args) {
+
+        // scrape and ingest
         List<String> listOfSubreddits = new ArrayList<>();
         listOfSubreddits.add("machinelearning");
-        FilteredSubredditIngestion ingestion = new FilteredSubredditIngestion(listOfSubreddits, 50);
-        ingestion.saveSubmissionAndMetadataAboveThresholdAsJson();
+        FilteredSubredditIngestion ingestion = new FilteredSubredditIngestion(listOfSubreddits, 1000);
+        
+        // the action to scrape and ingest
+        if(ingestion.isDataEmpty()) {
+            ingestion.saveSubmissionAndMetadataAboveThresholdAsJson();
+        }
 
+        // Spark setup
+        SparkSession spark = SparkSession.builder().appName("Reddit Post Classifier").master("local[4]").getOrCreate();
 
-//        SparkConf conf = new SparkConf().setAppName("Simple Application").setMaster("local[*]");
-//        JavaSparkContext sc = new JavaSparkContext(conf);
-//
-//        int NUM_SAMPLES = 100000000;
-//        List<Integer> l = new ArrayList<Integer>(NUM_SAMPLES);
-//        for (int i = 0; i < NUM_SAMPLES; i++) {
-//            l.add(i);
-//        }
-//
-//        long count = sc.parallelize(l).filter(new Function<Integer, Boolean>() {
-//            public Boolean call(Integer i) {
-//                double x = Math.random();
-//                double y = Math.random();
-//                return x*x + y*y < 1;
-//            }
-//        }).count();
-//        System.out.println("Pi is roughly " + 4.0 * count / NUM_SAMPLES);
-
+        // read into dataframe
+        String path = ingestion.getSparkDataPath();
+        Dataset<Row> data = spark.read().json(path).cache();
+        data.printSchema();
+        data.show();
+        System.out.println("number of entries: " + Long.toString(data.count()));
     }
 }
