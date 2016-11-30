eb32bd5a6cbae25db00905930845391ca454078d
hkiang01 <hkiang01@yahoo.com>
2016-11-30 12:45:02 -0500

    word freq

--- src/main/java/com/cs410dso/postclassifier/App.java
+++ src/main/java/com/cs410dso/postclassifier/App.java
@@ -1,16 +1,27 @@
 package com.cs410dso.postclassifier;
 
-import com.cs410dso.postclassifier.model.LocalSubredditFlairModel;
 import com.cs410dso.postclassifier.model.SubredditFlairModel;
 
+import org.apache.log4j.Level;
+import org.apache.log4j.LogManager;
 import org.apache.spark.api.java.function.Function;
+import org.apache.spark.ml.feature.CountVectorizer;
+import org.apache.spark.ml.feature.CountVectorizerModel;
+import org.apache.spark.ml.linalg.SparseVector;
 import org.apache.spark.sql.*;
-
-import java.util.ArrayList;
-import java.util.List;
 import org.apache.spark.ml.feature.RegexTokenizer;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
+import org.apache.spark.sql.api.java.UDF1;
+import org.apache.spark.sql.types.DataTypes;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
 /**
  * App main
  */
@@ -19,6 +30,8 @@ public class App {
 
         // Spark setup
         SparkSession spark = SparkSession.builder().appName("Reddit Post Classifier").master("local[4]").getOrCreate();
+        // https://stackoverflow.com/questions/31951728/how-to-set-up-logging-level-for-spark-application-in-intellij-idea
+        LogManager.getRootLogger().setLevel(Level.ERROR); // hide INFO
 
         // scrape and ingest
         ArrayList<String> listOfSubreddits = new ArrayList();
@@ -48,6 +61,7 @@ public class App {
                         "concat_ws( ' ', collect_list(text)) AS concat_text " +
                         "FROM data " +
                         "GROUP BY flair");
+        spark.sqlContext().dropTempTable("data");
         flairAndConcatText.show();
 
         // get the words out
@@ -74,6 +88,65 @@ public class App {
          +---------------+--------------------+--------------------+
          */
 
+        // https://stackoverflow.com/questions/34423281/spark-dataframe-word-count-per-document-single-row-per-document
+        final CountVectorizerModel cvModel = new CountVectorizer().setInputCol("words").setOutputCol("features").fit(flairAndWords);
+        final Dataset<Row> counted = cvModel.transform(flairAndWords);
+        counted.printSchema();
+        counted.show();
+
+        // debugging
+        int vocabLength = cvModel.vocabulary().length;
+        System.out.println("vocab length: " + vocabLength);
+
+        // the UDF for word counts
+        final String[] vocabulary = cvModel.vocabulary();
+        UDF1 wordFreqFromCountVectorizerModel = new UDF1<SparseVector, Map<String, Double>>() {
+            public Map<String, Double> call(SparseVector v) {
+                SparseVector sv = v.toSparse();
+                int length = sv.indices().length;
+                final double[] values = sv.values();
+                Map<String, Double> myMap = new HashMap<>();
+                for(int i = 0; i < length; i++) {
+                    myMap.put(vocabulary[i], values[i]);
+                }
+//                Arrays.stream(indices).forEachOrdered(i -> {
+//                    String vocabWord = vocabulary[i];
+//                    myMap.put(vocabWord, values[i]);
+//                });
+                return myMap;
+            }
+        };
+
+        // the SQL query for word counts
+        counted.registerTempTable("counted");
+        spark.sqlContext().udf().register("wordFreqFromCountVectorizerModel", wordFreqFromCountVectorizerModel, DataTypes.createMapType(DataTypes.StringType, DataTypes.DoubleType));
+        final Dataset<Row> withFreq = spark.sql(
+                "SELECT " +
+                        "flair, " +
+                        "concat_text, " +
+                        "words, " +
+                        "features, " +
+                        "wordFreqFromCountVectorizerModel(features) AS freq " +
+                        "FROM counted "
+        );
+
+        // resultant word count
+        withFreq.printSchema();
+        withFreq.show();
+
+        /**
+         * Example:
+         +-----+--------------------+--------------------+--------------------+--------------------+
+         |flair|         concat_text|               words|            features|                freq|
+         +-----+--------------------+--------------------+--------------------+--------------------+
+         |  two|Some of the lates...|[some, of, the, l...|(24211,[0,1,2,3,4...|Map(gans -> 9.0, ...|
+         | null|Hi, so I actually...|[hi, so, i, actua...|(24211,[0,1,2,3,4...|Map(serious -> 3....|
+         | four|This is a TensorF...|[this, is, a, ten...|(24211,[0,1,2,3,4...|Map(serious -> 1....|
+         |  one|I have implemente...|[i, have, impleme...|(24211,[0,1,2,3,4...|Map(gans -> 5.0, ...|
+         |three|DataGenCARS is a ...|[datagencars, is,...|(24211,[0,1,2,3,4...|Map(mikhailfranco...|
+         +-----+--------------------+--------------------+--------------------+--------------------+
+         */
 
     }
+
 }
